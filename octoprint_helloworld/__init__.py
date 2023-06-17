5699cf7cfdd45289fcf6a14c64241bc2cd1b1a51
AvaB1066 <81318924+AvaB1066@users.noreply.github.com>
2023-06-17 17:35:33 -0400

    Update __init__.py

--- octoprint_helloworld/__init__.py
+++ octoprint_helloworld/__init__.py
@@ -3,6 +3,16 @@ from __future__ import absolute_import
 
 import octoprint.plugin
 
+import RPi.GPIO as GPIO    # Import Raspberry Pi GPIO library
+from time import sleep     # Import the sleep function from the time module
+GPIO.setwarnings(False)    # Ignore warning for now
+GPIO.setmode(GPIO.BMC)   # Use physical pin numbering
+GPIO.setup(26, GPIO.OUT, initial=GPIO.HIGH)   # Set pin 8 to be an output pin and set initial value to low (off)
+
+
+
+
+
 class HelloWorldPlugin(octoprint.plugin.StartupPlugin,
                        octoprint.plugin.TemplatePlugin,
                        octoprint.plugin.SettingsPlugin,
@@ -24,7 +34,19 @@ class HelloWorldPlugin(octoprint.plugin.StartupPlugin,
 			js=["js/helloworld.js"],
 			css=["css/helloworld.css"],
 			less=["less/helloworld.less"]
+			
+			
+			
+while True:
+	GPIO.output(26, GPIO.HIGH) # Turn on
+	sleep(1)                  # Sleep for 1 second
+	GPIO.output(26, GPIO.LOW)  # Turn off
+	sleep(1)                  # Sleep for 1 second
 		)
+	
+	
+	
+	
 
 __plugin_name__ = "Hello World"
 __plugin_pythoncompat__ = ">=2.7,<4"
