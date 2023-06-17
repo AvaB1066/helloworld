31da04f8c459ed3104b547c369870efbd97b75aa
AvaB1066 <81318924+AvaB1066@users.noreply.github.com>
2023-06-17 16:07:26 -0400

    Update __init__.py

--- octoprint_helloworld/__init__.py
+++ octoprint_helloworld/__init__.py
@@ -2,11 +2,50 @@
 from __future__ import absolute_import
 
 import octoprint.plugin
+import RPi.GPIO as GPIO
+from flask import jsonify, request
+
 
 class HelloWorldPlugin(octoprint.plugin.StartupPlugin,
-                       octoprint.plugin.TemplatePlugin,
-                       octoprint.plugin.SettingsPlugin,
-                       octoprint.plugin.AssetPlugin):
+						  octoprint.plugin.ShutdownPlugin,
+						  octoprint.plugin.EventHandlerPlugin,
+						  octoprint.plugin.TemplatePlugin,
+						  octoprint.plugin.SettingsPlugin,
+						  octoprint.plugin.AssetPlugin,
+						  octoprint.plugin.BlueprintPlugin):
+	
+	
+	
+	
+	
+		def __init__(self):
+		self.i = 0
+		self._gpioup = 0
+		self._powerup = 0
+		self._onevents = ["OPERATIONAL"]
+		self._offevents = ["ERROR", "CLOSED_WITH_ERROR", ]
+
+
+		
+		
+		def _initgpio(self):
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setwarnings(False)
+		GPIO.setup(26, GPIO.OUT)
+		self._gpioup = 1
+		
+		
+def readtemperature(self, comm_instance, parsed_temperatures, *args, **kwargs):
+current_temp = parsed_temperatures['B'][0]
+
+while True:
+	if current_temp >=50:
+		GPIO.output(26,True)
+	else:
+		GPIO.output(26,False)
+
+
+
 	def on_after_startup(self):
 		self._logger.info("Hello World! (more: %s)" % self._settings.get(["url"]))
 
